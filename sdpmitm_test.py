a1a39f91ebbf658ee4b826cf7b0dd0664549e688
Conor <conorpp@vt.edu>
2015-09-04 14:05:10 -0400

    missing imports

--- sdpmitm_test.py
+++ sdpmitm_test.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
 # Run this to MiTM SDP
-# python sdpmitm_test.py <slave-bt-addr>
 
 import sys
 from btmitm_mitm import mitm_sdp
 
 if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage: ./sdpmitm_test.py <master-bt-addr> <slave-bt-addr>')
+        sys.exit(1)
     mitm_sdp(sys.argv[1],sys.argv[2])
