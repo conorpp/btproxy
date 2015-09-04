1d4a11ddbc7c666cf3b0849c27cd9fa1e3f97c0d
Conor <conorpp@vt.edu>
2015-09-04 14:03:31 -0400

    removed skip

--- argparser.py
+++ argparser.py
@@ -14,13 +14,11 @@ parser.add_argument('-s','--scan', help='Scan for advertising Bluetooth devices'
 parser.add_argument('-d','--detailed', help='Additionally lookup services when scanning', action='store_true')
 
 parser.add_argument('-a','--set-address', help='Address to set for Bluetooth adaptor (requires -b)', )
-parser.add_argument('-c','--set-class', help='Class to set for Bluetooth adaptor in hex, e.g. 0x89abde or 1234f6 (requires -b)', )
-parser.add_argument('-n','--set-name', help='Name to set for Bluetooth adaptor (requires -b)', )
+parser.add_argument('-n','--repair', help='Don\'t reuse existing paired connection', action='store_true')
 
 parser.add_argument('-b','--bluetooth', help='Select a Bluetooth adaptor', )
 parser.add_argument('-l','--list', help='List Adaptors', action='store_true')
 
-parser.add_argument('-k','--skip', help='Skip to connecting', action="store_true")
 parser.add_argument('-1','--master-name', help='Spoofed name to use for master adaptor', default=None)
 parser.add_argument('-2','--slave-name', help='Spoofed name to use for slave adaptor', default=None)
 parser.add_argument('-C','--slave-active', help='Spoofed slave adaptor will actively connect to'+
